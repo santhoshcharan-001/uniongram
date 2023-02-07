ccf801a9cea9c3a7af679e89691d8414447b79f4
santhoshcharan-001 <2019287@iiitdmj.ac.in>
2023-02-08 00:58:13 +0530

    Added personal work for deployment

--- profileapp/views.py
+++ profileapp/views.py
@@ -202,6 +202,6 @@ class user_posts(APIView):
 def submitCode(request):
     startAutomate()
     logs=""
-    with open("./profileapp/logs.txt","r") as f:
-        logs=f.read()
+    with open("./profileapp/logs.txt","r") as files:
+        logs=files.read()
     return JsonResponse({"logs":logs})
